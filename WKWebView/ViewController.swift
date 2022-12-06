a2c1f4c4c3340ddbd803685ad3329d12d29ff10f
jacquelineschmitz <jacquelineschmitz81@gmail.com>
2022-12-06 10:12:16 +0100

    ...

--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        webView.navigationDelegate = self
+        
+        searchBar.text = url.absoluteString
+        searchBar.delegate = self
+        searchBar.autocorrectionType = .no
     }
-
+    @IBAction func forwardPressed(_ sender: UIButton){
+        webView.goForward()
+    }
+    @IBAction func backwardPressed(_ sender: UIButton){
+        webView.goBack()
+    }
 
 }
 
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        self.searchBar.text = webView.url?.absoluteString
+    }
+}
+extension ViewController: UISearchBarDelegate{
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    let searchText = searchBar.text!
+    searchBar.resignFirstResponder()
+    let newURL = URL(string: "https://www.\(searchText).com")!
+    webView.load(URLRequest(url: newURL))
+    searchBar.text = newURL.absoluteString
+  }
+}
